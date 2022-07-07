5aa2bb50ed6efe3e54c486fa04098e48890fd617
Xavier <xavier194152@aol.com>
2022-07-07 18:45:16 -0400

    Add files via upload

--- 666666.ino
+++ 666666.ino
@@ -1,50 +1,5 @@
-/*
-  Buttons example
-  June 11, 2015
-  Copyright (C) 2015 David Martinez
-  All rights reserved.
-  This code is the most basic barebones code for showing how to use buttons in
-  Arduboy.
-
-  This library is free software; you can redistribute it and/or
-  modify it under the terms of the GNU Lesser General Public
-  License as published by the Free Software Foundation; either
-  version 2.1 of the License, or (at your option) any later version.
-*/
-
-//#include <Arduboy2.h>
-
-// Make an instance of arduboy used for many functions
-//Arduboy2 arduboy;
-
-// Variables for your game go here.
-//char title[] = "Press Buttons!";
-//byte x;
-//byte y;
-
-// Width of each charcter including inter-character space
-//#define CHAR_WIDTH 6
-
-// Height of each charater
-//#define CHAR_HEIGHT 8
-
-// To get the number of characters, we subtract 1 from the length of
-// the array because there will be a NULL terminator at the end.
-//#define NUM_CHARS (sizeof(title) - 1)
-
-// This is the highest value that x can be without the end of the text
-// going farther than the right side of the screen. We add one because
-// there will be a 1 pixel space at the end of the last character.
-// WIDTH and HEIGHT are defined in the Arduboy library.
-//#define X_MAX (WIDTH - (NUM_CHARS * CHAR_WIDTH) + 1)
-
-// This is the highest value that y can be without the text going below
-// the bottom of the screen.
-//#define Y_MAX (HEIGHT - CHAR_HEIGHT)
-
 #define ENCODER_USE_INTERRUPTS
 //#define ENCODER_DO_NOT_USE_INTERRUPTS
-//we have interuupt on all pins on PortB and PD0123
 #include "Encoder.h"
 
 //D8 - PA06 - DGI2 - EXTINT6
@@ -72,37 +27,28 @@
 
 Encoder EncX(1, 0);
 Encoder EncY(2, 5);
-Encoder EncZ(8, 9);
-Encoder EncRX(11, 10);
-Encoder EncRY(12, 13);
+//Encoder EncZ(8, 9);
+//Encoder EncRX(11, 10);
+//Encoder EncRY(12, 13);
 //Encoder EncRZ(6, 7);
-//ThumbPointer for Rx and Ry
 
-// This function runs once in your game.
-// use it for anything that needs to be set only once in your game.
 void setup() {
-  //initiate arduboy instance
-  //  arduboy.begin();
+
   delay(1000);
   SerialUSB.begin(9600);
 
-  // here we set the framerate to 30, we do not need to run at default 60 and
-  // it saves us battery life.
-  //  arduboy.setFrameRate(30);
-SerialUSB.print("hello");
-  // set x and y to the middle of the screen
-  //  x = (WIDTH / 2) - (NUM_CHARS * CHAR_WIDTH / 2);
-  //  y = (HEIGHT / 2) - (CHAR_HEIGHT / 2);
-      pinMode(0, INPUT_PULLUP);
-    pinMode(1, INPUT_PULLUP);
-        pinMode(2, INPUT_PULLUP);
-    pinMode(5, INPUT_PULLUP);
-        pinMode(8, INPUT_PULLUP);
-    pinMode(9, INPUT_PULLUP);
-        pinMode(10, INPUT_PULLUP);
-    pinMode(11, INPUT_PULLUP);
-        pinMode(12, INPUT_PULLUP);
-    pinMode(13, INPUT);
+  SerialUSB.print("hello");
+
+  pinMode(0, INPUT_PULLUP);
+  pinMode(1, INPUT_PULLUP);
+  pinMode(2, INPUT_PULLUP);
+  pinMode(5, INPUT_PULLUP);
+  //        pinMode(8, INPUT_PULLUP);
+  //    pinMode(9, INPUT_PULLUP);
+  //        pinMode(10, INPUT_PULLUP);
+  //    pinMode(11, INPUT_PULLUP);
+  //        pinMode(12, INPUT_PULLUP);
+  //    pinMode(13, INPUT);
 }
 short posX = 0;
 short posY = 0;
@@ -117,15 +63,14 @@ short nPosRX = 0;
 short nPosRY = 0;
 //short nPosRZ = 0;
 
-// our main game loop, this runs once every cycle/frame.
-// this is where our game logic goes.
+
 void loop() {
   nPosX = EncX.read();
   nPosY = EncY.read();
-  nPosX = EncZ.read();
-  nPosRX = EncRX.read();
-  nPosRY = EncRY.read();
-  
+  //  nPosX = EncZ.read();
+  //  nPosRX = EncRX.read();
+  //  nPosRY = EncRY.read();
+
   if ((posX != nPosX) || (posY != nPosY) || (posZ != nPosZ) || (posRX != nPosRX) || (posRY != nPosRY)) {
     if (posX != nPosX) {
       posX = nPosX;
@@ -162,6 +107,6 @@ void loop() {
     }
     else
       SerialUSB.print("  ");
-      SerialUSB.println();
+    SerialUSB.println();
   }
 }
